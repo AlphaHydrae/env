a4d9a08fc9fe008dd1acded416357548bac44913
AlphaHydrae <hydrae.alpha@gmail.com>
2013-04-29 16:27:58 +0200

    Gitenv config for development.

--- .gitenv.rb
+++ .gitenv.rb
@@ -1,5 +1,11 @@
 
-repo "~/Projects/env"
+repo "~/Projects/env" do
+  symlink dot_files
+  symlink('prompt_adam2_setup').to('/opt/local/share/zsh/5.0.2/functions')
+end
 
-symlink dot_files
-symlink('prompt_adam2_setup').to_abs('/opt/local/share/zsh/5.0.2/functions')
+from "~/Safebox/Config" do
+  symlink dot_files
+  symlink('config').from('.ssh').to('.ssh')
+  symlink('settings.xml').from('.m2').to('.m2') if File.directory?(File.expand_path('~/.m2'))
+end
