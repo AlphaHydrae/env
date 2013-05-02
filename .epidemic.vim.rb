cba6d61af085f4425e787e1b36bb016a175e919f
AlphaHydrae <hydrae.alpha@gmail.com>
2013-05-02 16:31:35 +0200

    Vim color schemes.

--- .epidemic.vim.rb
+++ .epidemic.vim.rb
@@ -5,6 +5,7 @@ add "git://github.com/ervandew/supertab.git"
 add "git://github.com/scrooloose/nerdtree.git"
 add "git://github.com/tpope/vim-markdown.git"
 add "git://github.com/rodjek/vim-puppet.git"
+add "git://github.com/flazz/vim-colorschemes.git"
 
 add "git://github.com/wincent/Command-T.git" do
   Dir.chdir "ruby/command-t"
