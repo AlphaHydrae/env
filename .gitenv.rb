90171156ee4d11a32457a0236b003a301b10d776
AlphaHydrae <hydrae.alpha@gmail.com>
2013-05-26 15:59:45 +0200

    Ansible directory.

--- .gitenv.rb
+++ .gitenv.rb
@@ -1,12 +1,14 @@
 
 repo "~/Projects/env" do
-  symlink dot_files.except('.zshconfig')
+  symlink dot_files.except('.zshconfig', '.bash_profile')
   copy('.zshconfig').once
+  copy('.bash_profile').once
   copy('prompt_adam2_setup').to('/opt/local/share/zsh/5.0.2/functions')
 end
 
 from "~/Safebox/Config" do
   symlink dot_files
+  symlink('ansible', as: '.ansible').to('~')
   symlink('config').from('.ssh').to('.ssh')
   symlink('settings.xml').from('.m2').to('.m2') if File.directory?(File.expand_path('~/.m2'))
 end
