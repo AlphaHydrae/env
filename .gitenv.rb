4b4edc4d15bff053f0600bc3d9f7abc9e2d50301
Simon Oulevay <simon.oulevay@gmail.com>
2014-06-14 14:22:32 +0200

    gitenv: check existence of config folder.

--- .gitenv.rb
+++ .gitenv.rb
@@ -9,9 +9,11 @@ repo "~/Projects/env" do
   copy('prompt_adam2_setup').to("/opt/local/share/zsh/#{zsh_version}/functions")
 end
 
-from "~/Safebox/Config" do
-  symlink dot_files.except('.env')
-  copy('.env').once
-  symlink('config').from('.ssh').to('.ssh')
-  symlink('settings.xml').from('.m2').to('.m2') if File.directory?(File.expand_path('~/.m2'))
+if File.directory? File.expand_path("~/Safebox/Config")
+  from "~/Safebox/Config" do
+    symlink dot_files.except('.env')
+    copy('.env').once
+    symlink('config').from('.ssh').to('.ssh')
+    symlink('settings.xml').from('.m2').to('.m2') if File.directory?(File.expand_path('~/.m2'))
+  end
 end
