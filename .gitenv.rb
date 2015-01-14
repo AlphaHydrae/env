1daa0481bc995d2979ebb5bdb4816e3ef916a3ca
Simon Oulevay <simon.oulevay@gmail.com>
2015-01-14 18:23:36 +0100

    Gitenv: removed hardcoded repo path.

--- .gitenv.rb
+++ .gitenv.rb
@@ -3,14 +3,10 @@ zsh_entries = Dir.entries([ '/opt/local/share/zsh', '/usr/share/zsh' ].find{ |pa
 zsh_version = zsh_entries.select{ |e| e.match /\A\d+\.\d+\.\d+\Z/ }.sort.last
 raise "Could not detect ZSH version" unless zsh_version
 
-raise "$ENV_REPO must be set" unless ENV['ENV_REPO']
-
-repo ENV['ENV_REPO'] do
-  symlink dot_files.except('.zshconfig', '.bash_profile')
-  copy('.zshconfig').once
-  copy('.bash_profile').once
-  copy('prompt_adam2_setup').to("/opt/local/share/zsh/#{zsh_version}/functions")
-end
+symlink dot_files.except('.zshconfig', '.bash_profile')
+copy('.zshconfig').once
+copy('.bash_profile').once
+copy('prompt_adam2_setup').to("/opt/local/share/zsh/#{zsh_version}/functions")
 
 if File.directory? File.expand_path("~/Safebox/Config")
   from "~/Safebox/Config" do
