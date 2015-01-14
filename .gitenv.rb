0028a2c11ec713223431f4a72ac4719535480ce6
Simon Oulevay <simon.oulevay@gmail.com>
2015-01-14 21:50:15 +0100

    Gitenv: fixed zsh prompt copy.

--- .gitenv.rb
+++ .gitenv.rb
@@ -1,12 +1,12 @@
 
-zsh_entries = Dir.entries([ '/opt/local/share/zsh', '/usr/share/zsh' ].find{ |path| File.directory? path })
-zsh_version = zsh_entries.select{ |e| e.match /\A\d+\.\d+\.\d+\Z/ }.sort.last
+zsh_path = [ '/opt/local/share/zsh', '/usr/share/zsh' ].find{ |path| File.directory? path }
+zsh_version = Dir.entries(zsh_path).select{ |e| e.match /\A\d+\.\d+\.\d+\Z/ }.sort.last
 raise "Could not detect ZSH version" unless zsh_version
 
 symlink dot_files.except('.zshconfig', '.bash_profile')
 copy('.zshconfig').once
 copy('.bash_profile').once
-copy('prompt_adam2_setup').to("/opt/local/share/zsh/#{zsh_version}/functions")
+copy('prompt_adam2_setup').to File.join(zsh_path, zsh_version, "functions")
 
 if File.directory? File.expand_path("~/Safebox/Config")
   from "~/Safebox/Config" do
