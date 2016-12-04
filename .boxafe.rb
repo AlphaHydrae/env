2c0b86696f68bf25e12df9114b8a00d070ddadb4
Simon Oulevay <simon.oulevay@gmail.com>
2016-12-04 15:06:53 +0100

    Boxafe: load mount env variable.

--- .boxafe.rb
+++ .boxafe.rb
@@ -2,4 +2,4 @@
 notify true
 
 Dotenv.load File.expand_path('~/.env')
-box env(:name, :root, :encfs_config, :password_file, prefix: :boxafe_)
+box env(:name, :root, :mount, :encfs_config, :password_file, prefix: :boxafe_)
