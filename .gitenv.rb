33125329920f456180e9a6b60bc65eca7d0bd4f4
Simon Oulevay <simon.oulevay@gmail.com>
2021-07-21 09:41:40 +0200

    Gitenv: include private configuration

--- .gitenv.rb
+++ .gitenv.rb
@@ -1,11 +1,14 @@
+# Dotfiles
 symlink dot_files.except('.gitmodules', '.zshlocal', '.bash_profile'), overwrite: true, backup: false
 copy('.zshlocal').once
 copy('.bash_profile').once
 
+# HTTPie
 from '.httpie' do
   copy('config.json').to('~/.httpie').once
 end
 
+# Visual Studio Code
 vscode_dir = File.expand_path(File.join('~', 'Library', 'Application Support', 'Code', 'User'))
 if File.directory? vscode_dir
   from 'vscode' do
@@ -14,6 +17,7 @@ if File.directory? vscode_dir
   end
 end
 
+# ZSH
 zsh_old_homebrew_path = '/usr/local/Cellar/zsh'
 zsh_homebrew_path = '/opt/homebrew/Cellar/zsh'
 zsh_homebrew_path = zsh_old_homebrew_path if File.directory? zsh_old_homebrew_path
@@ -23,11 +27,26 @@ if File.directory? zsh_homebrew_path
   copy('prompt_adam2_setup', overwrite: true).to File.join(zsh_homebrew_path, zsh_version, 'share', 'zsh', 'functions')
 end
 
-private_config_dir = ENV['PRIVATE_CONFIG']
-if private_config_dir && File.directory?(File.expand_path(private_config_dir))
-  from private_config_dir do
-    symlink dot_files.except('.env')
-    copy('.env').once
-    symlink('config').from('.ssh').to('.ssh')
+# Private configuration
+private_config_dir = ENV['PRIVATE_CONFIG_DIR']
+raise 'Environment variable $PRIVATE_CONFIG_DIR is required' unless private_config_dir
+from private_config_dir do
+  symlink dot_files.except('.env', '.gitenv.private.rb')
+  copy('.env').once
+
+  from '.config' do
+    symlink('rclone.conf').to('.config/rclone')
+  end
+
+  from '.m2' do
+    symlink('settings.xml').to('.m2')
+  end
+
+  from '.ssh' do
+    symlink('config').to('.ssh')
+    symlink('config.sync.d').to('.ssh')
   end
 end
+
+# Further private configuration
+include File.join(private_config_dir, '.gitenv.private.rb')
