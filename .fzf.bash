b863edcf4b359a12c477d73929b55b8f2ecfb52d
Simon Oulevay <simon.oulevay@gmail.com>
2021-12-02 10:07:46 +0100

    Fzf: make configuration apt-compatible

--- .fzf.bash
+++ .fzf.bash
@@ -1,14 +1,19 @@
 # Setup fzf
 # ---------
-HOMEBREW_PREFIX="$(brew --prefix)"
-if [[ ! "$PATH" == *${HOMEBREW_PREFIX}/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}${HOMEBREW_PREFIX}/opt/fzf/bin"
+if command -v brew &>/dev/null; then
+  # Homebrew
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ ! "$PATH" == *${HOMEBREW_PREFIX}/opt/fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}${HOMEBREW_PREFIX}/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  [[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.bash" 2> /dev/null
+
+  # Key bindings
+  source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.bash"
+elif test -d /usr/share/doc/fzf/examples; then
+  # APT
+  source /usr/share/doc/fzf/examples/key-bindings.bash
+  source /usr/share/doc/fzf/examples/completion.bash
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.bash"
--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,20 @@
 # Setup fzf
 # ---------
-HOMEBREW_PREFIX="$(brew --prefix)"
-if [[ ! "$PATH" == *${HOMEBREW_PREFIX}/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}${HOMEBREW_PREFIX}/opt/fzf/bin"
+if command -v brew &>/dev/null; then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ ! "$PATH" == *${HOMEBREW_PREFIX}/opt/fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}${HOMEBREW_PREFIX}/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh"
+elif test -d /usr/share/doc/fzf/examples; then
+  # APT
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+  source /usr/share/doc/fzf/examples/completion.zsh
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh"
