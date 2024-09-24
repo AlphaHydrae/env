d0aa360fddd413052cb8984ad4d3fa714265bcef
Simon Oulevay <simon.oulevay@gmail.com>
2024-09-24 16:53:38 +0200

    Ansible: disable cowsay

--- oh-my-zsh/env.zsh
+++ oh-my-zsh/env.zsh
@@ -11,5 +11,9 @@ export PROJECT_EDITOR="vim -c NERDTree"
 # GPG
 export GPG_TTY=$(tty)
 
+# Disable Ansible usage of cowsay
+# See https://github.com/ansible/ansible/issues/68571
+export ANSIBLE_NOCOWS=1
+
 # Increase the maximum number of open file descriptors
 ulimit -n 1024
